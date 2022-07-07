a7167e51a4c3b13565c554d928d875311132d6c1
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-07-07 20:51:29 -0300

    Vinculo Pedido e Rotulo

---
+++ ApiCerveja/DTO/PedidoDTO.cs
@@ -0,0 +1,16 @@
+﻿using Cerveja.Models.Enums;
+
+namespace ApiCerveja.DTO
+{
+    public class PedidoDTO
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Valor { get; set; }
+        public StatusPedido Status { get; set; }
+
+        // FK's
+        //public Vendedor Vendedor { get; set; }
+        //public int VendedorId { get; set; }
+    }
+}
