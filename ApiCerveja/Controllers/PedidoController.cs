347a0d1f7658e385bd28349e17f4f46de404f8ab
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-07-07 20:28:20 -0300

    Pedido controller e service

---
+++ ApiCerveja/Controllers/PedidoController.cs
@@ -0,0 +1,21 @@
+﻿using ApiCerveja.Services;
+using Microsoft.AspNetCore.Mvc;
+using Cerveja.Models;
+
+namespace ApiCerveja.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+
+        private readonly PedidoService _pedidoService;
+
+        public PedidoController(PedidoService pedidoService)
+        {
+            _pedidoService = pedidoService;
+        }
+
+        //------------------------------------------------------------------------------------------------------
+    }
+}
---
+++ ApiCerveja/Services/PedidoService.cs
@@ -0,0 +1,16 @@
+﻿using Cerveja.Models;
+using Cerveja.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCerveja.Services
+{
+    public class PedidoService
+    {
+        private readonly CervejaContext _context;
+
+        public PedidoService(CervejaContext context)
+        {
+            _context = context;
+        }
+    }
+}
