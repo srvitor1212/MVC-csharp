b496f55225144a0c8d2ca31b6ef0c84b39ac3c44
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-07-13 16:52:39 -0300

    Data Annotations, Departamento

--- Cerveja/Models/Departamento.cs
+++ Cerveja/Models/Departamento.cs
@@ -1,10 +1,14 @@
-﻿using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Cerveja.Models
 {
     public class Departamento
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O {0} deve ser informado")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "O tamanho deve ser entre {2} e {1} ")]
         public string Nome { get; set; }
 
 
