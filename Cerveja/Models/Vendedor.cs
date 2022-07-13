94319b9db615146c5a2d708406ed8bd20fc1a3ae
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-07-13 17:13:50 -0300

    Data Annotations, Vendedor, problema no Nome, não está dando msg de erro no front

--- Cerveja/Models/Vendedor.cs
+++ Cerveja/Models/Vendedor.cs
@@ -6,6 +6,9 @@ namespace Cerveja.Models
     public class Vendedor
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O {0} deve ser informado")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "O tamanho deve ser entre {2} e {1} ")]
         public string Nome { get; set; }
 
         [Display(Name = "Data Nascimento")]
@@ -15,8 +18,9 @@ namespace Cerveja.Models
         [Display(Name = "Salário")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Salario { get; set; }
-     
+
         // FK's
+        [Required(ErrorMessage = "O {0} deve ser informado")]
         public Departamento Departamento { get; set; } // Isso daqui já cria uma coluna "DepartamentoId" no DB
         public int DepartamentoId { get; set; }
 
