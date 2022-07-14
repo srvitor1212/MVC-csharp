2f5838d4b0578c22be160e2ce262b13c75bf312a
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-07-14 14:12:54 -0300

    Annotations Rotulos

--- Cerveja/Models/Rotulo.cs
+++ Cerveja/Models/Rotulo.cs
@@ -1,8 +1,14 @@
-﻿namespace Cerveja.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cerveja.Models
 {
     public class Rotulo
     {
         public int Id { get; set; }
+        
+        
+        [Required(ErrorMessage = "Este campo é Obrigatório")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "O tamanho deve ser entre {2} e {1} ")]
         public string Nome { get; set; }
 
 
