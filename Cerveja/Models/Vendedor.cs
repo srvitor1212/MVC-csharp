647ef4522c1ec34b46b5bc5b402eec18cf9e81ce
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-07-14 13:49:12 -0300

    Annotations de vendedor finalizado

--- Cerveja/Models/Vendedor.cs
+++ Cerveja/Models/Vendedor.cs
@@ -7,21 +7,26 @@ namespace Cerveja.Models
     {
         public int Id { get; set; }
 
+
         [Required(ErrorMessage = "O {0} deve ser informado")]
         [StringLength(150, MinimumLength = 3, ErrorMessage = "O tamanho deve ser entre {2} e {1} ")]
         public string Nome { get; set; }
 
+
+        [Required(ErrorMessage = "Este campo é Obrigatório")]
         [Display(Name = "Data Nascimento")]
         [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
 
+
+        [Required(ErrorMessage = "Este campo é Obrigatório")]
         [Display(Name = "Salário")]
-        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Salario { get; set; }
 
+
         // FK's
-        [Required(ErrorMessage = "O {0} deve ser informado")]
         public Departamento Departamento { get; set; } // Isso daqui já cria uma coluna "DepartamentoId" no DB
+        [Required(ErrorMessage = "O {0} deve ser informado")]
         public int DepartamentoId { get; set; }
 
 
