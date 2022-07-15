58f8e74f6de593bc303c14404faae55a6ea56545
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-07-15 15:08:19 -0300

    Refatorando - Adaptar código para estrutura de tópicos - Departamentos

--- Cerveja/Controllers/DepartamentosController.cs
+++ Cerveja/Controllers/DepartamentosController.cs
@@ -1,7 +1,7 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using Cerveja.Models.ViewModels;
 using Cerveja.Services;
-using Microsoft.AspNetCore.Mvc;
 using Cerveja.Models;
 using Cerveja.Services.Exceptions;
 
@@ -10,34 +10,21 @@ namespace Cerveja.Controllers
     public class DepartamentosController : Controller
     {
         private readonly DepartamentoService _departamentoService;
-
         public DepartamentosController(DepartamentoService departamentoService)
         {
             _departamentoService = departamentoService;
         }
 
-        //-----------------------------------------------------------------------------------------------------
+        // Method GET
         public IActionResult Index()
         {
             var list = _departamentoService.FindAll();
             return View(list);
         }
-
-        //-----------------------------------------------------------------------------------------------------
         public IActionResult Create()
         {
             return View();
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Departamento dep)
-        {
-            _departamentoService.Insert(dep);
-            return RedirectToAction(nameof(Index));
-        }
-
-        //-----------------------------------------------------------------------------------------------------
         public IActionResult Edit(int? id)
         {
             if (id == null)
@@ -49,27 +36,6 @@ namespace Cerveja.Controllers
 
             return View(dep);
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Departamento dep)
-        {
-            try
-            {
-                _departamentoService.Update(dep);
-                return RedirectToAction(nameof(Index));
-            } 
-            catch (ServiceNotFoundException e)
-            {
-                return RedirectToAction(nameof(Error), new { msg = e.Message });
-            }
-            catch (ServiceConcurrencyException e)
-            {
-                return RedirectToAction(nameof(Error), new { msg = e.Message });
-            }
-        }
-
-        //-----------------------------------------------------------------------------------------------------
         public IActionResult Details(int? id)
         {
             if (id == null)
@@ -81,8 +47,6 @@ namespace Cerveja.Controllers
 
             return View(obj);
         }
-
-        //-----------------------------------------------------------------------------------------------------
         public IActionResult Delete(int? id)
         {
             if (id == null)
@@ -95,15 +59,44 @@ namespace Cerveja.Controllers
             return View(obj);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+
+        // Method POST
+        [HttpPost] [ValidateAntiForgeryToken]
+        public IActionResult Create(Departamento dep)
+        {
+            _departamentoService.Insert(dep);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost] [ValidateAntiForgeryToken]
+        public IActionResult Edit(Departamento dep)
+        {
+            try
+            {
+                _departamentoService.Update(dep);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ServiceNotFoundException e)
+            {
+                return RedirectToAction(nameof(Error), new { msg = e.Message });
+            }
+            catch (ServiceConcurrencyException e)
+            {
+                return RedirectToAction(nameof(Error), new { msg = e.Message });
+            }
+        }
+
+
+        [HttpPost] [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
         {
             _departamentoService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
 
-        //-----------------------------------------------------------------------------------------------------
+
+        // Exceptions
         public IActionResult Error(string msg)
         {
             var viewModel = new ErrorViewModel {
